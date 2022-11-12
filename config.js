be91e178c162ad1356453e59ec7279a3bdfe50d0
112c <tobyfox2020@gmail.com>
2022-11-12 16:50:31 +0000

    Update config.js

--- config.js
+++ config.js
@@ -23,7 +23,7 @@ const CONFIG = {
 	greetingNight: 'Go to Sleep!',
 
 	// Layout
-	bentoLayout: 'bento', // 'bento', 'lists', 'buttons'
+	bentoLayout: 'buttons', // 'bento', 'lists', 'buttons'
 
 	// Weather
 	weatherKey: '3661f1a06b233d50014ef2ae610a6c88', // Write here your API Key
@@ -46,84 +46,186 @@ const CONFIG = {
 	hourDarkThemeActive: '18:30',
 	hourDarkThemeInactive: '07:00',
 
-	// ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
-	// ├┴┐│ │ │  │ │ ││││└─┐
-	// └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
 
-	firstButtonsContainer: [
-		{
-			id: '1',
-			name: 'Github',
-			icon: 'github',
-			link: 'https://github.com/',
-		},
-		{
-			id: '2',
-			name: 'Salmon Mail',
-			icon: 'mail',
-			link: 'https://mail.salmon.host/',
-		},
-		{
-			id: '3',
-			name: 'Gmail',
-			icon: 'at-sign',
-			link: 'https://gmail.com/',
-		},
-		{
-			id: '4',
-			name: 'Calendar',
-			icon: 'calendar',
-			link: 'https://calendar.google.com/calendar/r',
-		},
-		{
-			id: '5',
-			name: 'Classroom',
-			icon: 'graduation-cap',
-			link: 'https://classroom.google.com/h',
-		},
-		{
-			id: '6',
-			name: 'Jellyfin',
-			icon: 'film',
-			link: 'https://jelly.willow.112c.co.uk/',
-		},
-	],
-	secondButtonsContainer: [
-		{
-			id: '1',
-			name: 'Music',
-			icon: 'headphones',
-			link: 'https://open.spotify.com',
-		},
-		{
-			id: '2',
-			name: 'twitter',
-			icon: 'twitter',
-			link: 'https://twitter.com/',
-		},
-		{
-			id: '3',
-			name: 'Discord',
-			icon: 'message-square',
-			link: 'https://discord.com/app',
-		},
-		{
-			id: '4',
-			name: 'iCloud',
-			icon: 'cloud',
-			link: 'https://beta.icloud.com/',
-		},
-		{
-			id: '5',
-			name: 'Proxmox',
-			icon: 'x',
-			link: 'https://pmox.112c.co.uk/',
-		},
-		{
-			id: '6',
-			name: 'Spotify',
-			icon: 'music',
-			link: 'https://open.spotify.com/',
-		},
-	],
-};
+		// ┌┐ ┬ ┬┌┬┐┌┬┐┌─┐┌┐┌┌─┐
+		// ├┴┐│ │ │  │ │ ││││└─┐
+		// └─┘└─┘ ┴  ┴ └─┘┘└┘└─┘
+
+		firstButtonsContainer: [
+			{
+				id: '1',
+				name: 'Github',
+				icon: 'github',
+				link: 'https://github.com/',
+			},
+			{
+				id: '2',
+				name: 'Google Mail',
+				icon: 'at-sign',
+				link: 'https://mail.salmon.host/',
+			},
+			{
+				id: '3',
+				name: 'Mail',
+				icon: 'mail',
+				link: 'https://mail.salmon.host',
+			},
+			{
+				id: '4',
+				name: 'Calendar',
+				icon: 'calendar',
+				link: 'https://calendar.google.com/calendar/r',
+			},
+			{
+				id: '5',
+				name: 'Classroom',
+				icon: 'graduation-cap',
+				link: 'https://classroom.google.com/h',
+			},
+			{
+				id: '6',
+				name: 'Jellyfin',
+				icon: 'film',
+				link: 'https://jelly.willow.112c.co.uk/',
+			},
+		],
+
+		secondButtonsContainer: [
+			{
+				id: '1',
+				name: 'Music',
+				icon: 'headphones',
+				link: 'https://open.spotify.com',
+			},
+			{
+				id: '2',
+				name: 'Twitter',
+				icon: 'twitter',
+				link: 'https://twitter.com/',
+			},
+			{
+				id: '3',
+				name: 'Discord',
+				icon: 'bot',
+				link: 'https://discord.com/app',
+			},
+			{
+				id: '4',
+				name: 'iCloud',
+				icon: 'cloud',
+				link: 'https://beta.icloud.com/',
+			},
+			{
+				id: '5',
+				name: 'Proxmox',
+				icon: 'x',
+				link: 'https://pmox.112c.co.uk/',
+			},
+			{
+				id: '6',
+				name: 'Invivious',
+				icon: 'youtube',
+				link: 'https://uk1.watchtube.app/',
+			},
+		],
+
+		// ┬  ┬┌─┐┌┬┐┌─┐
+		// │  │└─┐ │ └─┐
+		// ┴─┘┴└─┘ ┴ └─┘
+
+		// First Links Container
+		firstlistsContainer: [
+			{
+				icon: 'music',
+				id: '1',
+				links: [
+					{
+						name: 'Inspirational',
+						link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+					},
+					{
+						name: 'Classic',
+						link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+					},
+					{
+						name: 'Oldies',
+						link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+					},
+					{
+						name: 'Rock',
+						link: 'https://www.youtube.com/watch?v=dQw4w9WgXcQ',
+					},
+				],
+			},
+			{
+				icon: 'coffee',
+				id: '2',
+				links: [
+					{
+						name: 'Linkedin',
+						link: 'https://www.linkedin.com',
+					},
+					{
+						name: 'Dribbble',
+						link: 'https://www.dribbble.com',
+					},
+					{
+						name: 'Trello',
+						link: 'https://www.trello.com',
+					},
+					{
+						name: 'Slack',
+						link: 'https://www.slack.com',
+					},
+				],
+			},
+		],
+
+		// Second Links Container
+		secondListsContainer: [
+			{
+				icon: 'binary',
+				id: '1',
+				links: [
+					{
+						name: 'Spotify',
+						link: 'https://www.spotify.com',
+					},
+					{
+						name: 'Reddit',
+						link: 'https://www.reddit.com',
+					},
+					{
+						name: 'Hashnode',
+						link: 'https://www.hashnode.com',
+					},
+					{
+						name: 'Pocket',
+						link: 'https://www.pocket.com',
+					},
+				],
+			},
+			{
+				icon: 'github',
+				id: '2',
+				links: [
+					{
+						name: 'Front',
+						link: 'https://www.reddit.com/r/Frontend/',
+					},
+					{
+						name: 'Rust',
+						link: 'https://www.reddit.com/r/rust/',
+					},
+					{
+						name: 'Go',
+						link: 'https://www.reddit.com/r/golang/',
+					},
+					{
+						name: 'Repos',
+						link: 'https://github.com/migueravila',
+					},
+				],
+			},
+		],
+	};
